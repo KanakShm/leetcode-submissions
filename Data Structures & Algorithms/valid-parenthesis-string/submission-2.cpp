6b9bef48b1fbc9d4f624400ac87758551d07189a
Kanak Sharma <kanak4768@gmail.com>
2026-04-09 23:57:24 +1000

    Add: valid-parenthesis-string - submission-2

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-2.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        /*
+            every time we see ( we append to a stack
+            if we see ), we pop from the stack
+
+            count the number of * we see and while we have ( on the stack we pop
+                if the stack gets empty before we run out of * that is good
+            
+            if the stack is empty and we still have to go through the string
+                if we see ), decrement a count from *
+            ***)
+
+            ***)))
+
+            *(
+        */
+
+        std::stack<int> parenthesis;
+        std::stack<int> stars;
+
+        int star_count = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '(') {
+                parenthesis.push(i);
+            }
+
+            if (s[i] == '*') {
+                stars.push(i);
+            }
+
+            if (s[i] == ')') {
+                if (parenthesis.empty()) {
+                    if (stars.empty()) return false;
+                    stars.pop();
+                } else {
+                    parenthesis.pop();
+                }
+            }
+        }
+
+        while (!parenthesis.empty() && !stars.empty()) {
+            if (stars.top() > parenthesis.top()) {
+                parenthesis.pop();
+                stars.pop();
+            } else {
+                break;
+            }
+        }
+
+        return parenthesis.empty();
+    }
+};
