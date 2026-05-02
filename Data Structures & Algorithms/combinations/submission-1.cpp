995e00ca111f83151b289176817b440acb6cefec
Kanak Sharma <kanak4768@gmail.com>
2026-05-02 11:26:02 +1000

    Add: combinations - submission-1

---
+++ Data Structures & Algorithms/combinations/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    std::vector<std::vector<int>> res;
+    vector<vector<int>> combine(int n, int k) {
+        /*
+        Backtracking with i = 1 -> n
+        for loop starting at j = i -> Only looking forward
+        dfs with j + 1 ->only Cant choose itself again
+        base case when combination array reaches k size add to res array and return
+        */
+
+        std::vector<int> combination = {};
+        dfs(n, 1, combination, k);
+        return res;
+    }
+
+    void dfs(int n, int i, std::vector<int>& combination, int k) {
+        if (combination.size() == k) {
+            res.push_back(combination);
+            return;
+        }
+
+        for (int j = i; j <= n; ++j) {
+            combination.push_back(j);
+            dfs(n, j + 1, combination, k);
+            combination.pop_back();
+        }
+    }
+};
